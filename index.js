ec10026ae62f9e4a1768219b063c63962499b43f
Stephanie Kayode <stephanie.kayode@gmail.com>
2023-11-27 21:13:34 +0000

    change from mousover to click

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 window.onload= function(){ //tells script to only perform functions once the windows loads, otherwise may get error
 const about_me = document.querySelector("#about_me");
 
-about_me.addEventListener("mouseover", showAboutMeSection, {once:true});
+about_me.addEventListener("click", showAboutMeSection, {once:true});
 
 function showAboutMeSection(){
     const div = document.querySelector("#content");
@@ -23,37 +23,55 @@ function showAboutMeSection(){
     x.setAttribute("alt", "Picture of code");
     x.style.position = "absolute";
     x.style.left = 695+'px';
-    x.style.top = 400 +'px';
+    x.style.top = 320 +'px';
     document.body.appendChild(x);
+        event.preventDefault();
 }
 
 
 const skills = document.querySelector("#skills");
-skills.addEventListener("mouseover", showSkillsSection, {once:true});
+skills.addEventListener("click", showSkillsSection, {once:true});
 
 function showSkillsSection(){
     const div2 = document.querySelector("#content2");
     const paragraph2 = document.createElement("p");
-    const content2 = document.createTextNode ('- HTML\n' + '- CSS\n' + '- Javascript\n' + ' - Git\n' + '- Github\n' + '- Python\n' + '- Command line');
+    const content2 = document.createTextNode ("• HTML " + "\u00A0\u00A0\u00A0" + '• CSS '+ "\u00A0\u00A0\u00A0" + '• Javascript ' + "\u00A0\u00A0\u00A0" + ' • Git ' + "\u00A0\u00A0\u00A0" + '• Github '+ "\u00A0\u00A0\u00A0" + '• Python ' + "\u00A0\u00A0\u00A0"+ '• Command line');
 
     paragraph2.appendChild(content2);
     div2.appendChild(paragraph2);
     document.body.appendChild(div2);
-    
+    event.preventDefault();
 }
 
 
-const projects = document.querySelector("#list_projects");
-projects.addEventListener("mouseover", showProjectsSection, {once:true});
+const courses = document.querySelector("#courses");
+courses.addEventListener("click", showCoursesSection, {once:true});
 
-function showProjectsSection(){
+
+function showCoursesSection(){
     const div3 = document.querySelector("#content3");
     const paragraph3 = document.createElement("p");
-    const content3 = document.createTextNode ("- QR code generator  " + "  -Password generator  " + "  - PDF merger");
+    const content3 = document.createTextNode ("• Introduction to Web Development: Code First Girl's " + "\u00A0\u00A0\u00A0\u00A0" + "• Introduction to Data and SQL: Code First Girl's " + "\u00A0\u00A0\u00A0\u00A0" + "• Python: Code First Girls"+ "• Learn Python 3: Codecademy" + "\u00A0\u00A0\u00A0\u00A0" + "• Introduction to web development: CBF" + "\u00A0\u00A0\u00A0\u00A0" + "• Introduction to Programming with Python: CS50, Harvard");
 
     paragraph3.appendChild(content3);
     div3.appendChild(paragraph3);
     document.body.appendChild(div3);
+    event.preventDefault();
+}
+
+
+const projects = document.querySelector("#list_projects");
+projects.addEventListener("click", showProjectsSection, {once:true});
+
+function showProjectsSection(){
+    const div4 = document.querySelector("#content4");
+    const paragraph4 = document.createElement("p");
+    const content4 = document.createTextNode ("• QR code generator  " + "\u00A0\u00A0\u00A0\u00A0" + "• Password generator  " + "\u00A0\u00A0\u00A0\u00A0" + "• PDF merger");
+
+    paragraph4.appendChild(content4);
+    div4.appendChild(paragraph4);
+    document.body.appendChild(div4);
+    event.preventDefault();
 }
 
 
